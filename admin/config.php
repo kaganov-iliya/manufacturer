e9873806d756a614b95f54291f4eb130e77aa944
kaganov_iliya <kaganov.iliya@.com>
2015-08-11 21:24:25 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://manufacturer/admin/');
+define('HTTP_CATALOG', 'http://manufacturer/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://manufacturer/admin/');
+define('HTTPS_CATALOG', 'http://manufacturer/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\OpenServer\domains\manufacturer/admin/');
+define('DIR_SYSTEM', 'D:\OpenServer\domains\manufacturer/system/');
+define('DIR_DATABASE', 'D:\OpenServer\domains\manufacturer/system/database/');
+define('DIR_LANGUAGE', 'D:\OpenServer\domains\manufacturer/admin/language/');
+define('DIR_TEMPLATE', 'D:\OpenServer\domains\manufacturer/admin/view/template/');
+define('DIR_CONFIG', 'D:\OpenServer\domains\manufacturer/system/config/');
+define('DIR_IMAGE', 'D:\OpenServer\domains\manufacturer/image/');
+define('DIR_CACHE', 'D:\OpenServer\domains\manufacturer/system/cache/');
+define('DIR_DOWNLOAD', 'D:\OpenServer\domains\manufacturer/download/');
+define('DIR_LOGS', 'D:\OpenServer\domains\manufacturer/system/logs/');
+define('DIR_CATALOG', 'D:\OpenServer\domains\manufacturer/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'manufacturer');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://manufacturer/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://manufacturer/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\OpenServer\domains\manufacturer/catalog/');
+define('DIR_SYSTEM', 'D:\OpenServer\domains\manufacturer/system/');
+define('DIR_DATABASE', 'D:\OpenServer\domains\manufacturer/system/database/');
+define('DIR_LANGUAGE', 'D:\OpenServer\domains\manufacturer/catalog/language/');
+define('DIR_TEMPLATE', 'D:\OpenServer\domains\manufacturer/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\OpenServer\domains\manufacturer/system/config/');
+define('DIR_IMAGE', 'D:\OpenServer\domains\manufacturer/image/');
+define('DIR_CACHE', 'D:\OpenServer\domains\manufacturer/system/cache/');
+define('DIR_DOWNLOAD', 'D:\OpenServer\domains\manufacturer/download/');
+define('DIR_LOGS', 'D:\OpenServer\domains\manufacturer/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'manufacturer');
+define('DB_PREFIX', 'oc_');
+?>
